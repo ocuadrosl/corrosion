fc6a04f2033a95c139bfeaf95a88cff786d1a2e5
ocuadrosl <ocuadrosl@gmail.com>
2018-09-15 19:47:27 -0300

    creating and writing mesh baefore metrics computation

--- src/CorrosionAxial.cpp
+++ src/CorrosionAxial.cpp
@@ -18,15 +18,15 @@ int main(int argc, const char* argv[])
 {
 
 	std::string inputFile;
-	if(argc == 2)
-	{		
+	if (argc == 2)
+	{
 		inputFile = argv[1];
 	}
 	else
 	{
 		//inputFile = "input/electrode5_Imagens_tratadas/electrode5Tratadas.txt";
-		std::cerr<<"ERROR: Configuration file is required"<<std::endl;
-		return 0; 
+		std::cerr << "ERROR: Configuration file is required" << std::endl;
+		return 0;
 	}
 
 	Interface interface(inputFile);
@@ -47,9 +47,14 @@ int main(int argc, const char* argv[])
 	type::grayImagePointer3D outputImage = image3D.getImage3D();
 	type::grayImagePointer3D outputSegmentedImage = image3D.getSegmentedImage3D();
 
+	//writing images
 	io::writeImage<type::grayImageType3D>(outputImage, interface.getOutputDir() + "/" + interface.getTestName() + ".nii");
 	io::writeImage<type::grayImageType3D>(outputSegmentedImage, interface.getOutputDir() + "/segmented" + interface.getTestName() + ".nii");
 
+	//creating and writing mesh
+	std::string vtkName = interface.getOutputDir() + "/" + interface.getTestName() + ".vtk";
+	type::meshTypePointer mesh = ip::extractIsoSurface(outputSegmentedImage, 1);
+	io::writeMesh(mesh, vtkName);
 
 //reading volume tmp
 //type::grayImagePointer3D outputImage = io::readImage<type::grayImageType3D>("output/volume.nii");
@@ -67,10 +72,8 @@ int main(int argc, const char* argv[])
 	 type::grayImagePointer3D segmentedImage = ip::connectedThresholdFilter<type::grayImageType3D>(outputImage, seed);
 	 */
 
-	
-
-	if(interface.getStatistics3D())
-	{	
+	if (interface.getStatistics3D())
+	{
 
 		//computing connected components
 		typedef itk::LabelObject<type::grayImageType3D::PixelType, 3> labelObjectType3D;
@@ -80,28 +83,22 @@ int main(int argc, const char* argv[])
 		std::string rgbName = interface.getOutputDir() + "/" + interface.getTestName() + "rgb.nii";
 		typename labelMapType3D::Pointer labelMap3D = ip::connectedComponents<type::grayImageType3D, labelMapType3D>(outputSegmentedImage, rgbName);
 
-		ip::computeLabelMapStatisticsMuiltiThread(labelMap3D, interface.getOutputDir() + "/" + interface.getTestName()+"Metrics.csv" );
-	
+		ip::computeLabelMapStatisticsMuiltiThread(labelMap3D, interface.getOutputDir() + "/" + interface.getTestName() + "Metrics.csv");
+
 	}
-	
-	if(interface.getStatistics2D())
+
+	if (interface.getStatistics2D())
 	{
 
 		//computing connected components slice by slice
-		utils::computeStatistics2D(image3D.getImageSeries(), interface.getOutputDir() + "/" + interface.getTestName()+"MetricsBySlice.csv" );
-	
-	}
-	
-	if(interface.getStatistics2DInput())	
-	{
-		utils::computeStatistics2D(image3D.getInputImageSeries(), interface.getOutputDir()+ "/"+interface.getTestName()+"MetricsBySliceInput.csv");
-	}
-	
+		utils::computeStatistics2D(image3D.getImageSeries(), interface.getOutputDir() + "/" + interface.getTestName() + "MetricsBySlice.csv");
 
-	//creating mesh
-	std::string vtkName = interface.getOutputDir() + "/" + interface.getTestName() + ".vtk";
-	type::meshTypePointer mesh = ip::extractIsoSurface(outputSegmentedImage, 1);
-	io::writeMesh(mesh, vtkName);
+	}
+
+	if (interface.getStatistics2DInput())
+	{
+		utils::computeStatistics2D(image3D.getInputImageSeries(), interface.getOutputDir() + "/" + interface.getTestName() + "MetricsBySliceInput.csv");
+	}
 
 	std::cout << "DONE" << std::endl;
 
